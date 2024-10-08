591c0c9198673c3941cf83b80542355d3523eca9
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-07 17:40:51 -0700

    Program to swap the variables in 2 integers x,y using pointers.

---
+++ SwapVariablesUsingPointers/swap.cpp
@@ -0,0 +1,8 @@
+//
+//  swap.cpp
+//  SwapVariablesUsingPointers
+//
+//  Created by Chatchapong Thuaypha on 10/7/24.
+//
+
+#include <stdio.h>
---
+++ SwapVariablesUsingPointers/swap.h
@@ -0,0 +1,12 @@
+//
+//  swap.h
+//  SwapVariablesUsingPointers
+//
+//  Created by Chatchapong Thuaypha on 10/7/24.
+//
+
+#ifndef swap_h
+#define swap_h
+
+
+#endif /* swap_h */
